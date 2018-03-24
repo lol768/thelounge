4584fbf78c47d6965513fddfadd2575efdea25ad
Adam Williams <lol768@lol768.com>
2018-03-24 16:44:04 +0000

    Add auto-prepend behaviour for unprefixed channels
    This change adds behaviour to automatically prefix channel names passed in via the "?join=x,y,z" query string/search parameter which do not appear to include an appropriate channel symbol.

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -585,7 +585,20 @@ $(function() {
 			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in#Iterating_over_own_properties_only
 			for (let key in params) {
 				if (params.hasOwnProperty(key)) {
-					const value = params[key];
+					let value = params[key];
+
+					if (key === "join") {
+						let channels = value.split(",");
+						channels = channels.map((c) => {
+							if (c.match(/^\w/)) {
+								return "#" + c;
+							}
+
+							return c;
+						});
+						value = channels.join(",");
+					}
+
 					// \W searches for non-word characters
 					key = key.replace(/\W/g, "");
 
